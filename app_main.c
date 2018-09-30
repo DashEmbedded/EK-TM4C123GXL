24b4167dad352bc02f41a48145275b22509ef9b1
DashEmbedded <43716727+DashEmbedded@users.noreply.github.com>
2018-09-29 23:58:44 -0700

    Fix alignment

--- app_main.c
+++ app_main.c
@@ -31,12 +31,12 @@ void led_test2()
 {
 	while(1)
 	{
-			GPIO_led_toggle(LED_RED, 900000);
-			bsp_delay(5000000);
-			GPIO_led_toggle(LED_BLUE, 900000);
-			bsp_delay(5000000);
-			GPIO_led_toggle(LED_GREEN, 900000);
-			bsp_delay(5000000);		
+		GPIO_led_toggle(LED_RED, 900000);
+		bsp_delay(5000000);
+		GPIO_led_toggle(LED_BLUE, 900000);
+		bsp_delay(5000000);
+		GPIO_led_toggle(LED_GREEN, 900000);
+		bsp_delay(5000000);		
 	}
 }
 
@@ -49,41 +49,41 @@ void button_test()
 	
 	while(1)
 	{		
-			button_state = GPIO_pin_read(GPIO_BUTTON_PORT, ALL_BUTTONS);
-			
-			// switches are connected weak pull up meaning, 
-			// release = 1
-			// pressed = 0
-		
-			if((button_state & BUTTON_RIGHT) == PRESSED)
-			{
-				GPIO_pin_write(GPIO_LED_PORT, LED_RED, 1);
-				bsp_delay(2000000);
-				GPIO_pin_write(GPIO_LED_PORT, LED_BLUE, 1);
-				bsp_delay(2000000);
-				GPIO_pin_write(GPIO_LED_PORT, LED_GREEN, 1);
-				bsp_delay(2000000);
-			}
-			else if((button_state & BUTTON_LEFT) == PRESSED)
-			{
-				GPIO_pin_write(GPIO_LED_PORT, LED_GREEN, 1);
-				bsp_delay(2000000);
-				GPIO_pin_write(GPIO_LED_PORT, LED_BLUE, 1);
-				bsp_delay(2000000);
-				GPIO_pin_write(GPIO_LED_PORT, LED_RED, 1);
-				bsp_delay(2000000);
-			}
-			else
-			{
-				GPIO_pin_write(GPIO_LED_PORT, 0, 1);
-			}
+		button_state = GPIO_pin_read(GPIO_BUTTON_PORT, ALL_BUTTONS);
+
+		// switches are connected weak pull up meaning, 
+		// release = 1
+		// pressed = 0
+
+		if((button_state & BUTTON_RIGHT) == PRESSED)
+		{
+			GPIO_pin_write(GPIO_LED_PORT, LED_RED, 1);
+			bsp_delay(2000000);
+			GPIO_pin_write(GPIO_LED_PORT, LED_BLUE, 1);
+			bsp_delay(2000000);
+			GPIO_pin_write(GPIO_LED_PORT, LED_GREEN, 1);
+			bsp_delay(2000000);
+		}
+		else if((button_state & BUTTON_LEFT) == PRESSED)
+		{
+			GPIO_pin_write(GPIO_LED_PORT, LED_GREEN, 1);
+			bsp_delay(2000000);
+			GPIO_pin_write(GPIO_LED_PORT, LED_BLUE, 1);
+			bsp_delay(2000000);
+			GPIO_pin_write(GPIO_LED_PORT, LED_RED, 1);
+			bsp_delay(2000000);
+		}
+		else
+		{
+			GPIO_pin_write(GPIO_LED_PORT, 0, 1);
+		}
 	}
 }
 
 int main(void)
 {
 	// set up the system clock
-  bsp_clock_set(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+	bsp_clock_set(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	
 	//configure the led gpio ports & pins 
 	GPIO_led_configure(GPIO_BUS_APB);
@@ -93,5 +93,7 @@ int main(void)
 	
 	//led_test1();
 	button_test();
+	
+	
 	return 0;
 }
